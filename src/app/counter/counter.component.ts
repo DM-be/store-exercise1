3a4154e0e484486fe4e16951f4fed81253c15a11
DM-be <dennis.morent@student.howest.be>
2020-10-12 18:03:26 +0200

    4 files modified

--- src/app/counter/counter.component.ts
+++ src/app/counter/counter.component.ts
@@ -1,24 +1,25 @@
-import { Component, OnInit } from '@angular/core';
-import { Store } from '@ngrx/store';
-import { Observable } from 'rxjs';
-import { AppState } from '../store/app.state';
-import { IncrementCounterAction } from '../store/count.action';
-import { CountState } from '../store/count.state';
+import { Component, OnInit } from "@angular/core";
+import { Store } from "@ngrx/store";
+import { Observable } from "rxjs";
+import { AppState } from "../store/app.state";
+import {
+  DecrementCounterAction,
+  IncrementCounterAction,
+  ResetCounterAction
+} from "../store/count.action";
 
 @Component({
-  selector: 'app-counter',
-  templateUrl: './counter.component.html',
-  styleUrls: ['./counter.component.css']
+  selector: "app-counter",
+  templateUrl: "./counter.component.html",
+  styleUrls: ["./counter.component.css"]
 })
 export class CounterComponent implements OnInit {
-
-
   public count$: Observable<number>;
 
-  constructor(private store: Store<AppState>) { }
+  constructor(private store: Store<AppState>) {}
 
   ngOnInit() {
-    this.count$ = this.store.select(state =>  state.countState.count);
+    this.count$ = this.store.select(state => state.countState.count);
   }
 
   public dispatchIncrement(): void {
@@ -27,7 +28,13 @@ export class CounterComponent implements OnInit {
 
   // dispatch decrement action
 
+  public dispatchDecrement(): void {
+    this.store.dispatch(new DecrementCounterAction());
+  }
+
   // dispatch reset action
 
-
+  public resetCounter(): void {
+    this.store.dispatch(new ResetCounterAction());
+  }
 }
--- src/app/store/count.action.ts
+++ src/app/store/count.action.ts
@@ -3,13 +3,23 @@ import { Action } from "@ngrx/store";
 
 // add new enums
 export enum CountActionTypes  {
-  INCREMENT_COUNT = '[COUNT] increment counter'
+  INCREMENT_COUNT = '[COUNT] increment DECREMENT_COUNT',
+  DECREMENT_COUNT = '[COUNT] decrement count',
+  RESET_COUNT = '[COUNT] reset count'
 }
 
 export class IncrementCounterAction implements Action {
    readonly type = CountActionTypes.INCREMENT_COUNT;
 }
 
+export class DecrementCounterAction implements Action {
+  readonly type = CountActionTypes.DECREMENT_COUNT;
+}
+
+export class ResetCounterAction implements Action {
+  readonly type = CountActionTypes.RESET_COUNT
+}
+
 // add more actions to this type
 // syntax: IncrementCounterAction | DecrementCounterAction | ...
-export type CountAction = IncrementCounterAction; // |
+export type CountAction = IncrementCounterAction | DecrementCounterAction | ResetCounterAction
